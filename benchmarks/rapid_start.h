196035a0c28ee532c614200e3584d75f3cc776a0
Anton Malakhov <Anton.Malakhov@intel.com>
2021-09-14 10:23:59 +0300

    Implemented rapid start

--- benchmarks/rapid_start.h
+++ benchmarks/rapid_start.h
@@ -2,14 +2,22 @@
 #pragma once
 #include <algorithm>
 #include <atomic>
-#include <tbb/task.h>
+#if __has_include(<zmmintrin.h>)
+#include <zmmintrin.h>
+#define _clevict(a, b) _mm_clevict(a, b)
+#else
+#define _clevict(a, b)
+#endif
+
+#include <tbb/task_group.h>
 
 typedef uintptr_t mask_t;
 
 namespace Harness {
 const size_t MAX_THREADS = sizeof(mask_t)*8;
+using tbb_task = tbb::detail::d1::task;
 
-class distribution_base : tbb::internal::no_assign {
+class distribution_base {
 public:
     virtual void execute(int part, int parts) const = 0;
     virtual ~distribution_base() {}
@@ -42,75 +50,76 @@ public:
     distribution_function ( int s, int e, F& f ) : my_start(s), my_end(e), my_func(f) {}
 };
 struct mask1 {
-    tbb::atomic<mask_t> start_mask;
+    std::atomic<mask_t> start_mask;
 };
 struct mask2 {
-    tbb::atomic<mask_t> finish_mask;
+    std::atomic<mask_t> finish_mask;
 };
 struct work_ {
-    tbb::atomic<mask_t> run_mask;
+    std::atomic<mask_t> run_mask;
     distribution_base *func_ptr;
-    tbb::atomic<uintptr_t> epoch;
+    std::atomic<uintptr_t> epoch;
     volatile int mode; // 0 - stopping, 1 - rebalance, 2 - trapped
 };
-struct __attribute__((aligned(64))) RapidStart : tbb::internal::padded<mask1>, tbb::internal::padded<mask2>, tbb::internal::padded<work_> {
-    tbb::atomic<uintptr_t> n_tasks;
+struct __attribute__((aligned(64))) RapidStart : tbb::detail::padded<mask1>, tbb::detail::padded<mask2>, tbb::detail::padded<work_> {
+    std::atomic<uintptr_t> n_tasks;
+    tbb::task_group tg;
 
     friend class TrapperTask;
 
     void spread_work(distribution_base *f) {
         uintptr_t e = epoch;
-        run_mask.store<tbb::relaxed>(0U);
+        run_mask.store(0U, std::memory_order_relaxed);
         func_ptr = f;
-        epoch.store<tbb::release>(e+1);
+        epoch.store(e+1, std::memory_order_release);
         //tbb::atomic_fence();
         __asm__ __volatile__("lock; addl $0,(%%rsp)":::"memory");
-        mask_t mask_snapshot = start_mask.load<tbb::acquire>();
-        finish_mask.store<tbb::relaxed>(0);
-        run_mask.store<tbb::release>(mask_snapshot|1);
-        _mm_clevict(&finish_mask, _MM_HINT_T0);
+        mask_t mask_snapshot = start_mask.load(std::memory_order_acquire);
+        finish_mask.store(0, std::memory_order_relaxed);
+        run_mask.store(mask_snapshot|1, std::memory_order_release);
+        _clevict(&finish_mask, _MM_HINT_T0);
+
         f->run(0, mask_snapshot);
-        tbb::internal::spin_wait_until_eq(finish_mask, mask_snapshot);
+        tbb::detail::spin_wait_until_eq(finish_mask, mask_snapshot);
     }
 
-    class TrapperTask : public tbb::task {
-        tbb::task* execute () {
+    struct TrapperTask {
+        void operator() () const {
             __TBB_ASSERT(slot,0);
             const mask_t bit = mask_t(1)<<slot;
             if(global.mode) {
-                global.start_mask.fetch_and_add(bit);
-                uintptr_t e = global.epoch.load<tbb::relaxed>();
-                mask_t r = global.run_mask.load<tbb::acquire>();
+                global.start_mask.fetch_add(bit);
+                uintptr_t e = global.epoch.load(std::memory_order_relaxed);
+                mask_t r = global.run_mask.load(std::memory_order_acquire);
                 do {
                     if(r&bit) {
                         global.func_ptr->run(slot, r);
-                        __TBB_AtomicOR(&global.finish_mask, bit);
-                        _mm_clevict(&global.finish_mask, _MM_HINT_T1);
+                        global.finish_mask.fetch_or(bit);
+                        _clevict(&global.finish_mask, _MM_HINT_T1);
                     }
-                    tbb::internal::spin_wait_while_eq(global.epoch, e);
+                    tbb::detail::spin_wait_while_eq(global.epoch, e);
                     e = global.epoch;
                     _mm_prefetch((const char*)global.func_ptr, _MM_HINT_T0);
-                    tbb::internal::spin_wait_while_eq(global.run_mask, 0U);
+                    tbb::detail::spin_wait_while_eq(global.run_mask, 0U);
                     r = global.run_mask;
                 } while( (r&bit)==bit || global.mode == 2);
                 //printf("#%d exited r=%lu mode=%d\n", slot, r, global.mode );
-                global.start_mask.fetch_and_add(-bit);
+                global.start_mask.fetch_add(-bit);
                 // are we were late to leave the group
-                if( e != global.epoch.load<tbb::relaxed>() ) {
-                    tbb::internal::spin_wait_while_eq(global.run_mask, 0U);
+                if( e != global.epoch.load(std::memory_order_relaxed) ) {
+                    tbb::detail::spin_wait_while_eq(global.run_mask, 0U);
                     r = global.run_mask;
                     if(r&bit) {
                         global.func_ptr->run(slot, r);
-                        __TBB_AtomicOR(&global.finish_mask, bit);
-                        _mm_clevict(&global.finish_mask, _MM_HINT_T1);
+                        global.finish_mask.fetch_or(bit);
+                        _clevict(&global.finish_mask, _MM_HINT_T1);
                     }
                 }
             }
             if(global.mode == 0)
                 global.n_tasks--;
             else
-                tbb::task::enqueue( *new(allocate_root()) TrapperTask(slot, global) );
-            return NULL;
+                global.tg.run( TrapperTask(slot, global) );
         }
         RapidStart &global;
         const int slot;
@@ -126,9 +135,9 @@ public:
     void init ( int maxThreads = MAX_THREADS ) {
         n_tasks = maxThreads;
         if( n_tasks > 60/*MAX_THREADS*/ ) n_tasks = 60/*MAX_THREADS*/;
-#if 0
+#if 1
         for ( int i = 1; i < maxThreads; ++i )
-            tbb::task::enqueue( *new(tbb::task::allocate_root()) TrapperTask(i, *this) );
+            tg.run( TrapperTask(i, *this) );
 #else
         tbb::task_list  tl;
         for ( int i = 1; i < n_tasks; ++i ) {
@@ -144,7 +153,8 @@ public:
         mode = 0;
         run_mask = 1;
         epoch++;
-        tbb::internal::spin_wait_until_eq(n_tasks, 0U);
+        //tbb::detail::spin_wait_until_eq(n_tasks, 0U);
+        tg.wait();
     }
 
     template <typename Body>
